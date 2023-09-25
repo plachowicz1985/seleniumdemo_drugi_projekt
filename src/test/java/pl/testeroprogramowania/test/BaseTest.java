7ed529d2d7293d117891d77ab60db2458f3f7782
plachowicz1985 <piotr.lachowicz1985@gmail.com>
2023-09-25 19:56:12 +0200

    add tests and page objects

--- src/test/java/pl/testeroprogramowania/test/BaseTest.java
+++ src/test/java/pl/testeroprogramowania/test/BaseTest.java
@@ -1,7 +1,6 @@
 package pl.testeroprogramowania.test;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pl.testeroprogramowania.utils.DriverFactory;
